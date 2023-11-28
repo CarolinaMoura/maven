c868c4ec4acb4c3d613a5cf6305093885505270d
Ege Kabasakaloglu <egekabas@mit.edu>
2023-11-28 14:34:11 -0500

    Initial work on sections

---
+++ server/concepts/section.ts
@@ -0,0 +1,43 @@
+import { ObjectId } from "mongodb";
+import DocCollection, { BaseDoc } from "../framework/doc";
+
+export interface SectionDoc extends BaseDoc {
+  text: string;
+}
+
+export default class SectionConcept {
+  private readonly sections = new DocCollection<SectionDoc>("sections");
+
+  async createSection(text: string) {
+    await this.sections.createOne({ text });
+    return { msg: "Created section!" };
+  }
+  async getSections() {
+    return await this.sections.readMany({});
+  }
+  async getSection(id: ObjectId) {
+    const section = await this.sections.readOne({ _id: id });
+    if (section === null) {
+      throw new Error("Section not found!");
+    }
+    return section;
+  }
+
+  async splitIntoSections(text: string) {
+    const sentences = text.split(/(?<=[.?!])\s+(?=[A-Z])/);
+    const sections = [];
+    let curSection = "";
+    for (const sentence of sentences) {
+      curSection += sentence;
+      if (curSection.length > 400) {
+        sections.push(curSection);
+        curSection = "";
+      }
+    }
+    if (curSection.length > 0) {
+      sections.push(curSection);
+    }
+
+    return await Promise.all(sections.map((section) => this.sections.createOne({ text: section })));
+  }
+}
