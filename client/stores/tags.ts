d765becd1ceb579cf5532c57059e9e986550e40f
Kelly Fang <kellyfangg@gmail.com>
2023-11-30 19:23:02 -0500

    removed unused ref in tagStore

--- client/stores/tags.ts
+++ client/stores/tags.ts
@@ -7,14 +7,13 @@ export const useTagStore = defineStore(
   "tag",
   () => {
     const languageTags = ref([]);
-    const contentTags = ref([]);
 
     const getLanguageTags = async () => {
       const languageTagDocs = await fetchy(`/api/tag/language`, "GET");
       languageTags.value = languageTagDocs.map((t: Tag) => t.name);
     };
 
-    return { languageTags, contentTags, getLanguageTags };
+    return { languageTags, getLanguageTags };
   },
   { persist: true },
 );
