541cc034bc7caa6c7ef5039f1dee8a046e3b8510
Anna Murphy <almurph@mit.edu>
2023-12-04 23:13:30 -0500

    add button color to indicate toggling votes

--- client/components/Vote/VoteComponent.vue
+++ client/components/Vote/VoteComponent.vue
@@ -7,34 +7,42 @@ import { fetchy } from "../../utils/fetchy";
 const props = defineProps(["section", "votes"]);
 const emit = defineEmits(["refreshVotes", "refreshSectionTranslations"]);
 
-let totalVotes = ref(props.votes);
+let downSelected = ref(false);
+let upSelected = ref(false);
 const { isLoggedIn } = storeToRefs(useUserStore());
 
 const vote = async (upvote: boolean) => {
   try {
+    if (upvote) upSelected.value = !upSelected.value;
+    if (!upvote) downSelected.value = !downSelected.value;
     await fetchy("/api/votes/vote", "POST", {
       body: { section: props.section._id, upvote: upvote },
     });
     emit("refreshVotes");
     emit("refreshSectionTranslations");
-
   } catch {
     return new Error("Error voting");
   }
 };
+
+const getButtonColor = (upvote: boolean) => {
+  if (upvote && upSelected.value) return "success";
+  else if (!upvote && downSelected.value) return "success";
+  return "white"
+}
 </script>
 
 <template>
   <section v-if="isLoggedIn">
     <div class="vote-container">
-      <v-btn size="x-small" @click="vote(true)" color="success" icon>
+      <v-btn size="x-small" @click="vote(true)" :color="getButtonColor(true)" icon>
         <v-icon size="20">{{ "mdi-chevron-up" }}</v-icon>
       </v-btn>
       <div class="vote-count">
         <span>{{ props.votes }}</span>
       </div>
-      <v-btn size="x-small" @click="vote(false)" color="success" icon>
-        <v-icon size="20">{{ "mdi-chevron-down" }}</v-icon>
+      <v-btn size="x-small" @click="vote(false)" :color="getButtonColor(false)" icon>
+        <v-icon size=" 20">{{ "mdi-chevron-down" }}</v-icon>
       </v-btn>
     </div>
   </section>
