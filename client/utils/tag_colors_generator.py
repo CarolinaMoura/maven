4d008a99db4df32e8f7c0bc2dc12a8723fcc17f7
Carolina <carolmou@mit.edu>
2023-12-06 17:55:15 -0500

    Add real abbreviations for languages

--- client/utils/tag_colors_generator.py
+++ client/utils/tag_colors_generator.py
@@ -1,4 +1,5 @@
 import random 
+import iso639
 
 random.seed(10)
 
@@ -12,61 +13,11 @@ def random_color_generator():
 
 
 
-for lang in [
-    "English",
-    "Chinese",
-    "Spanish",
-    "Hindi",
-    "Arabic",
-    "Portuguese",
-    "Bengali",
-    "Russian",
-    "Japanese",
-    "Punjabi",
-    "French",
-    "German",
-    "Italian",
-    "Dutch",
-    "Swedish",
-    "Polish",
-    "Greek",
-    "Romanian",
-    "Spanish",
-    "Portuguese",
-    "Chinese",
-    "Hindi",
-    "Bengali",
-    "Japanese",
-    "Punjabi",
-    "Telugu",
-    "Marathi",
-    "Tamil",
-    "Urdu",
-    "Turkish",
-    "Swahili",
-    "Hausa",
-    "Yoruba",
-    "Amharic",
-    "Zulu",
-    "Igbo",
-    "Shona",
-    "Somali",
-    "Fulfulde",
-    "Tigrinya",
-    "Arabic",
-    "Persian (Farsi)",
-    "Turkish",
-    "Hebrew",
-    "Kurdish",
-    "Quechua",
-    "Maori",
-    "Ainu",
-    "Navajo",
-  ]:
-    print(f"['{lang}', ", end="")
+for lang in iso639.ALL_LANGUAGES:
+    if lang.status == 'R':
+        continue
+    print(f'["{lang.name}", ', end="")
     print("{", end="")
-    str = lang[:3]
-    str = str.upper()
     color = random_color_generator()
-    print(f"abbreviation: '{str}', color: '{color}'", end="") 
+    print(f"abbreviation: '{lang.part3.upper()}', color: '{color}'", end="") 
     print("}],")
